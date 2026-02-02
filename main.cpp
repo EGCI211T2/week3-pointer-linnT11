62d0fb77142dba7724a62497818e07519caac1d3
linnT11 <linthet.nyi@student.mahidol.edu>
2026-02-02 11:20:52 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <iomanip>
 
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb,  temp;
   int n,i;
-  cout<<"how many numbers that you need?";
-  cin>> n;
+  
+  n = argc -1;
   pa = new int[n];
-for (i=0;i<n;i++){
-  cout<<"input"<<i+1<<": ";
-  cin>> pa[i];
-}
+  int *base = pa;
+//for (i=0;i<n;i++){
+ // cout<<"input"<<i+1<<": ";
+ // cin>> pa[i];
+for (i = 0; i < n; i++) {
+        pa[i] = atoi(argv[i + 1]);
+    }
 
 cout<<"Original: ";
-for (i=0; i<n-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
+for (i=0; i<n; i++ ){
+   cout<<setw(3)<<base[i] ;//<<endl;
    
 
-  //cout<<pa<<endl;
+  
 }
-cout<<setw(3)<<*pa<<endl;
+cout<<endl;
+//cout<<setw(3)<<*pa<<endl;
 
-pa -= (n-1);  
-pb = pa + n - 1;
+//pa -= (n-1);  
+//pb = pa + n - 1;
 
-for (i=0; i<n/2; i++)
-{
+//for (i=0; i<n/2; i++)
+/*{
    temp = *pa;  
    *pa = *pb;  
    *pb = temp;
@@ -45,4 +50,21 @@ cout<<setw(3)<<*pa<<endl;
 delete [] pa;
 
 return 0;
+}*/
+pa = base + n - 1;  
+pb = base; 
+cout<<"Reversed: ";
+pb=pa-n+1;
+for (i=0;i<n/2;i++){
+  temp= *pa;
+  *pa= *pb;
+  *pb=temp;
+  pa--;
+  pb++;
+
+}
+for (i=0; i<n; i++ ){
+   cout<<setw(3)<<base[i] ;
+}
+delete [] base;
 }
